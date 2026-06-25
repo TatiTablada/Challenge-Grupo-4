f58c3261655813024ac4a18b53a43cf27122583a
Tati Tablada <tatitablada14@gmail.com>
2026-06-25 20:02:31 -0300

    Incorporación de comentarios en archivo de reservas.

--- cypress/e2e/reservasInicio.cy.js
+++ cypress/e2e/reservasInicio.cy.js
@@ -8,9 +8,14 @@ describe('Sección Rooms', () => {
         // RESERVAS-012
 
         it('Click en "Book now" en las habitaciones - Double', () => {
+            // Paso 1: Hace scroll hacia abajo y verifica que la sección de habitaciones sea visible
             cy.scrollTo(0, 1200);
             cy.get('#rooms > .container').should('be.visible')
+
+            // Paso 2: Busca la segunda habitación ("Double") y hace click en su botón de reserva
             cy.get(':nth-child(2) > .card > .card-footer > .btn').click()
+
+            // Paso 3: Valida que se abra el detalle de la habitación "Double Room" y cargue el formulario
             cy.get('.breadcrumb').should('be.visible').and('contain', 'Double Room')
             cy.get('.col-lg-8').should('be.visible')
             cy.get('.shadow > :nth-child(1)').should('be.visible')
